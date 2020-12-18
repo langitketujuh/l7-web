d904cf7d1ac8624bf91f30fef1ecdd43a9e8eb4a
hervyqa <hervyqa@protonmail.com>
2020-12-18 13:18:47 +0700

    add: script ms fonts installer. better script

--- static/sh/ms-corefont.sh
+++ static/sh/ms-corefont.sh
@@ -4,35 +4,47 @@ mkdir -p /tmp/ms-corefonts/
 mkdir -p $HOME/.fonts/ms-corefonts/
 cd /tmp/ms-corefonts/
 
-wget -c "https://sourceforge.net/projects/corefonts/files/the%20fonts/final/wd97vwr32.exe/download" -O wd97vwr32.exe
-wget -c "https://sourceforge.net/projects/corefonts/files/the%20fonts/final/webdin32.exe/download" -O webdin32.exe
-wget -c "https://sourceforge.net/projects/corefonts/files/the%20fonts/final/verdan32.exe/download" -O verdan32.exe
-wget -c "https://sourceforge.net/projects/corefonts/files/the%20fonts/final/trebuc32.exe/download" -O trebuc32.exe
-wget -c "https://sourceforge.net/projects/corefonts/files/the%20fonts/final/times32.exe/download" -O times32.exe
-wget -c "https://sourceforge.net/projects/corefonts/files/the%20fonts/final/impact32.exe/download" -O impact32.exe
-wget -c "https://sourceforge.net/projects/corefonts/files/the%20fonts/final/georgi32.exe/download" -O georgi32.exe
-wget -c "https://sourceforge.net/projects/corefonts/files/the%20fonts/final/courie32.exe/download" -O courie32.exe
-wget -c "https://sourceforge.net/projects/corefonts/files/the%20fonts/final/comic32.exe/download" -O comic32.exe
-wget -c "https://sourceforge.net/projects/corefonts/files/the%20fonts/final/arialb32.exe/download" -O arialb32.exe
-wget -c "https://sourceforge.net/projects/corefonts/files/the%20fonts/final/arial32.exe/download" -O arial32.exe
-wget -c "https://sourceforge.net/projects/corefonts/files/the%20fonts/final/andale32.exe/download" -O andale32.exe
+echo ">>> Downloading webdin32 fonts..."
+wget -c -nv "https://sourceforge.net/projects/corefonts/files/the%20fonts/final/webdin32.exe/download" -O webdin32.exe
+echo ">>> Downloading verdan32 fonts..."
+wget -c -nv "https://sourceforge.net/projects/corefonts/files/the%20fonts/final/verdan32.exe/download" -O verdan32.exe
+echo ">>> Downloading trebuc32 fonts..."
+wget -c -nv "https://sourceforge.net/projects/corefonts/files/the%20fonts/final/trebuc32.exe/download" -O trebuc32.exe
+echo ">>> Downloading times32 fonts..."
+wget -c -nv "https://sourceforge.net/projects/corefonts/files/the%20fonts/final/times32.exe/download" -O times32.exe
+echo ">>> Downloading impact32 fonts..."
+wget -c -nv "https://sourceforge.net/projects/corefonts/files/the%20fonts/final/impact32.exe/download" -O impact32.exe
+echo ">>> Downloading georgi32 fonts..."
+wget -c -nv "https://sourceforge.net/projects/corefonts/files/the%20fonts/final/georgi32.exe/download" -O georgi32.exe
+echo ">>> Downloading courie32 fonts..."
+wget -c -nv "https://sourceforge.net/projects/corefonts/files/the%20fonts/final/courie32.exe/download" -O courie32.exe
+echo ">>> Downloading comic32 fonts..."
+wget -c -nv "https://sourceforge.net/projects/corefonts/files/the%20fonts/final/comic32.exe/download" -O comic32.exe
+echo ">>> Downloading arialb32 fonts..."
+wget -c -nv "https://sourceforge.net/projects/corefonts/files/the%20fonts/final/arialb32.exe/download" -O arialb32.exe
+echo ">>> Downloading arial32 fonts..."
+wget -c -nv "https://sourceforge.net/projects/corefonts/files/the%20fonts/final/arial32.exe/download" -O arial32.exe
+echo ">>> Downloading andale32 fonts..."
+wget -c -nv "https://sourceforge.net/projects/corefonts/files/the%20fonts/final/andale32.exe/download" -O andale32.exe
 
-7z -y x wd97vwr32.exe
-7z -y x webdin32.exe
-7z -y x verdan32.exe
-7z -y x trebuc32.exe
-7z -y x times32.exe
-7z -y x impact32.exe
-7z -y x georgi32.exe
-7z -y x courie32.exe
-7z -y x comic32.exe
-7z -y x arialb32.exe
-7z -y x arial32.exe
-7z -y x andale32.exe
+echo ">>> Extract fonts..."
+7z -y x webdin32.exe >/dev/null 2>&1
+7z -y x verdan32.exe >/dev/null 2>&1
+7z -y x trebuc32.exe >/dev/null 2>&1
+7z -y x times32.exe >/dev/null 2>&1
+7z -y x impact32.exe >/dev/null 2>&1
+7z -y x georgi32.exe >/dev/null 2>&1
+7z -y x courie32.exe >/dev/null 2>&1
+7z -y x comic32.exe >/dev/null 2>&1
+7z -y x arialb32.exe >/dev/null 2>&1
+7z -y x arial32.exe >/dev/null 2>&1
+7z -y x andale32.exe >/dev/null 2>&1
 
+echo ">>> Moving fonts..."
 mv *.TTF $HOME/.fonts/ms-corefonts/
 mv *.ttf $HOME/.fonts/ms-corefonts/
 
+echo ">>> Refresh fonts..."
 fc-cache -f
 
 echo ">>> FINISH"
