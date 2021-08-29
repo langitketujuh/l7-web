a076ebc1ae18f82b7264d42fea36a8c680bff526
Hervy Qurrotul Ainur Rozi <hervyqa@pm.me>
2021-08-29 14:07:54 +0700

    add: testimoni slider
    - remove id form

--- assets/js/script.js
+++ assets/js/script.js
@@ -23,4 +23,35 @@
 		arrows: false
 	});
 
+
+		/* ========================================================================= */
+		/*	Testimonial Carousel
+		/* =========================================================================  */
+
+		//Init the slider
+		$('.testimonial-slider').slick({
+			slidesToShow: 3,
+			slidesToScroll: 1,
+			infinite: true,
+			arrows: false,
+			autoplay: true,
+			autoplaySpeed: 2000,
+			responsive: [{
+					breakpoint: 600,
+					settings: {
+						slidesToShow: 1,
+						slidesToScroll: 2
+					}
+				},
+				{
+					breakpoint: 480,
+					settings: {
+						slidesToShow: 1,
+						slidesToScroll: 1
+					}
+				}
+			]
+		});
+
 })(jQuery);
+
